71b7a53c1d09d6c90e7825104b8823026b116039
murat.sivil <murat.sivil@outlook.com>
2021-01-10 01:21:07 +0300

    I added two more example in Singleton Design Pattern.

---
+++ Singleton/Example2/SingletonClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton.Second_Example
+{
+    public class SingletonClass
+    {
+        private SingletonClass()
+        {
+              // By making constructor private, let's prevent an instance from being taken from outside this class.
+        }
+
+        private static SingletonClass _object = new SingletonClass();
+        //We have defined property to provide external access to the static object that we have created.
+
+        public static SingletonClass Class
+        {
+            get { return _object; }
+        }
+
+        // Or we can do the same thing with function.
+        public static SingletonClass Class_2()
+        {
+            return _object;
+        }
+
+        public int Summation(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ Singleton/Example3/SingletonClass_2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton.Example3
+{
+    public class SingletonClass_2
+    {
+        private static SingletonClass_2 _singleObject;
+
+        private static object _lockObject = new object();
+
+        public SingletonClass_2()
+        {
+
+        }
+        public static SingletonClass_2 Class
+        {
+            get
+            {
+                if (_singleObject == null)
+                {
+                    lock (_lockObject)
+                    {
+                        if (_singleObject == null)
+                        {
+                            _singleObject = new SingletonClass_2();
+                        }
+                    }
+                }
+
+                return _singleObject;
+            }
+        }
+    }
+}
